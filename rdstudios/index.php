b5fcbd2d3de1da4edb069334a2c13167e60bdc11
Raja Dewangan <Raja@rajadewangan0842.com>
2023-05-23 06:05:28 +0530

    display category is completed

--- rdstudios/index.php
+++ rdstudios/index.php
@@ -7,6 +7,33 @@ require_once("header.php");
     <a href="loginForm.php">Login &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</a>
     <a href="customerForm.php">New User</a>
   </div>
+
+  <div id='categoryContainer'>
+    <?php
+  if(isset($_REQUEST["cid"]))
+    $prid=$_REQUEST['cid'];
+  else 
+    $prid=0;
+
+    require_once("dbconnect.php");
+    $sql = "SELECT * FROM `category_info` where cat_parent_id=$prid order by cat_dname";
+    $rsCat = mysqli_query($con,$sql) or die("Query Error !");
+    while($row=mysqli_fetch_array($rsCat))
+  {
+     $id=$row['cat_id'];
+     $cname=$row['cat_dname'];
+     echo("<div class='category'>");
+     echo("<a href='index.php?cid=$id'>");
+     echo($cname."<br><br>");
+     $img=$row['image_path'];
+     
+     echo("<img src='.\\collection\\$img' width='100' height='100' border='1'> ");
+     echo("</a>");
+     echo("</div>");
+
+  }
+  ?>
+</div><!--end of categoryContainer-->
 </div><!--end of container -->
 
 
